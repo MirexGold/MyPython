a55afde274400e66bc2005c6752f4a7e2c930d6e
Олег <us_sr@mail,ru>
2022-07-19 16:02:25 +0700

    7.16 Декораторы в Python Часть 1 + добавление файла

---
+++ Decorators.py
@@ -0,0 +1,10 @@
+# def func(*args, **kwargs):
+#     print(args)
+#     print(kwargs)
+#
+#
+# func(5, 2, 3,q='!', d='oleg', a=1, b=2, c=3)
+#
+# def func(a,b,...., *args,| kw_a=1, kw_b=2,...., **kwargs):
+
+
