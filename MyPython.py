8efe11f75d13194370ef3a450f7b0573f89eed0e
Олег <us_sr@mail,ru>
2022-07-21 12:53:45 +0700

    10.1 Выражения-генераторы

--- MyPython.py
+++ MyPython.py
@@ -6088,7 +6088,6 @@ cправа налево сверху вниз и вывести элемент�
 
 """Напишите декоратор double_it, который возвращает удвоенный результат вызова декорированной функции"""
 
-
 # def double_it(func):
 #     def inner(*args, **kwargs):
 #         return (func(*args, **kwargs)+func(*args, **kwargs))
@@ -6257,15 +6256,14 @@ cправа налево сверху вниз и вывести элемент�
 #     # file.close()
 
 
-
-#менеджер контекста
+# менеджер контекста
 # with open('test.txt', 'w') as f:
 #     f.write('1,3,4')
 #     f.write('hello')
 # print('end')
 
 
-#вывод размера папки. (если '.' - текущий каталог где лежит файл,
+# вывод размера папки. (если '.' - текущий каталог где лежит файл,
 # '..' - на уровень выше)
 # import os
 #
@@ -6276,7 +6274,7 @@ cправа налево сверху вниз и вывести элемент�
 # ----------------------------------------
 # 9.3 Работаем с JSON в Python. Парсинг JSON, сохраняем JSON в файл
 
-#JSON - JavaScript Object Notation
+# JSON - JavaScript Object Notation
 #
 # import json
 # from random import randint
@@ -6379,16 +6377,6 @@ cправа налево сверху вниз и вывести элемент�
 # print(data)
 #
 # --------------------------------------
-import json
-with open("manager_sales.json", 'r') as file:  # загружаем наши данные из файлика в переменную data
-    data = json.load(file)
-print(data)
-
-
-
-
-
-
 
 # import json
 # with open('manager_sales.json','r') as file:
@@ -6401,6 +6389,77 @@ print(data)
 #         name_manager= item['manager']['first_name']
 #         last_name_manager = item['manager']['last_name']
 # print(name_manager,last_name_manager,total_price)
+# ----------------------------------------------
+# import json
+#
+# with open('group_people.json', 'r') as file:
+#     data = json.load(file)
+#     maximum = 0
+#     max_group = None
+#     for group in data:
+#         id_group = group['id_group']
+#         count = 0
+#         for person in group['people']:
+#             if person['gender'] == 'Female' and person['year'] > 1977:
+#                 count+=1
+#         if count > maximum:
+#             maximum = count
+#             max_group = id_group
+#         # print(id_group, count)
+#     print(max_group, maximum)
+
+# --------------------------------------------
+# import json
+#
+# with open("Alphabet.json") as f:
+#     key = json.load(f)
+# with open("Abracadabra.txt", encoding="utf-8") as f:
+#     txt = f.read()
+# print(txt.translate(txt.maketrans(key)))
+# # --------------------
+# import json
+#
+# with open('Alphabet.json') as dict:
+#     d = json.load(dict)
+#
+# with open('Abracadabra.txt') as text:
+#     t = text.read()
+#
+# for i in t:
+#     if i in d:
+#         print(d[i], end='')
+#     else:
+#         print(i, end='')
+
+# --------------------------------------
+# import json
+#
+# people = '[{"name": "Haley Whitney", "country": "British Indian Ocean Territory (Chagos Archipelago)", "age": 54}, {"name": "Matthew King", "country": "Colombia", "age": 34}, {"name": "Sean Sullivan", "country": "Mayotte", "age": 40}, {"name": "Christian Crawford", "country": "Russian Federation", "age": 29}, {"name": "Sarah Contreras", "country": "Honduras", "age": 82}, {"name": "Danielle Williams", "country": "Togo", "age": 91}, {"name": "Jonathan Wilson", "country": "Tunisia", "age": 49}, {"name": "Patricia Wilkerson", "country": "Georgia", "age": 22}, {"name": "Zachary Scott", "country": "Brunei Darussalam", "age": 55}, {"name": "Elizabeth Sanchez", "country": "Nauru", "age": 23}, {"name": "Christina Fernandez", "country": "Burundi", "age": 71}, {"name": "Allen Norton", "country": "Montserrat", "age": 79}, {"name": "Scott Arroyo", "country": "Montenegro", "age": 72}, {"name": "Brooke Boyd", "country": "Latvia", "age": 74}, {"name": "Jerry Morrow", "country": "San Marino", "age": 23}, {"name": "Danielle Bradshaw", "country": "Vietnam", "age": 64}, {"name": "Jerry Thompson", "country": "Belgium", "age": 30}, {"name": "Mark Jordan", "country": "Comoros", "age": 89}, {"name": "Joseph Berger", "country": "Cook Islands", "age": 94}, {"name": "Gina Brooks", "country": "Samoa", "age": 51}, {"name": "Walter Duran", "country": "Chad", "age": 67}, {"name": "John Martinez", "country": "Wallis and Futuna", "age": 65}, {"name": "Johnny Glover", "country": "Eritrea", "age": 72}, {"name": "Lindsay Moore", "country": "Liberia", "age": 53}, {"name": "Kimberly Burton", "country": "Nicaragua", "age": 92}, {"name": "Jacqueline Ballard", "country": "Nigeria", "age": 78}, {"name": "Charles Thompson", "country": "Saudi Arabia", "age": 50}, {"name": "Suzanne Roberts", "country": "Serbia", "age": 43}, {"name": "David Decker", "country": "South Africa", "age": 71}, {"name": "Christopher Perez", "country": "Cayman Islands", "age": 49}, {"name": "Debra Hall", "country": "Greece", "age": 13}, {"name": "John King", "country": "Bahamas", "age": 40}, {"name": "Justin Galvan", "country": "Namibia", "age": 19}, {"name": "Jacqueline Berger", "country": "Yemen", "age": 59}, {"name": "Shawn Robinson", "country": "Saint Pierre and Miquelon", "age": 32}, {"name": "Kristen Garcia", "country": "Portugal", "age": 48}, {"name": "Christopher Barry", "country": "French Polynesia", "age": 23}, {"name": "Alejandra Cook", "country": "Egypt", "age": 16}, {"name": "Jill Harrell", "country": "Comoros", "age": 49}, {"name": "Sara Zimmerman", "country": "Brazil", "age": 26}, {"name": "Mrs. Charlene Flores", "country": "New Caledonia", "age": 75}, {"name": "Melissa Crawford", "country": "Lebanon", "age": 17}, {"name": "Larry Wong", "country": "New Caledonia", "age": 6}, {"name": "Brenda Acosta", "country": "Grenada", "age": 48}, {"name": "Latoya Terry", "country": "Saint Martin", "age": 41}, {"name": "Seth Luna", "country": "Sao Tome and Principe", "age": 59}, {"name": "Micheal Adams", "country": "Barbados", "age": 53}, {"name": "Susan Carroll", "country": "Somalia", "age": 64}, {"name": "Douglas Morris", "country": "Thailand", "age": 24}, {"name": "Dennis Wagner", "country": "Zimbabwe", "age": 66}, {"name": "Kristin Johnson", "country": "Niue", "age": 71}, {"name": "Steven Krause", "country": "Turkmenistan", "age": 84}, {"name": "Jared Smith", "country": "Colombia", "age": 46}, {"name": "Lauren Anderson", "country": "Christmas Island", "age": 46}, {"name": "Joshua Spencer", "country": "Russian Federation", "age": 38}, {"name": "Maria Edwards", "country": "Hungary", "age": 78}, {"name": "Anne Lee", "country": "United States of America", "age": 10}, {"name": "James Mckenzie", "country": "Uganda", "age": 43}, {"name": "Joshua Gallegos", "country": "United States Minor Outlying Islands", "age": 27}, {"name": "Paul Herrera", "country": "Kiribati", "age": 17}, {"name": "Veronica White", "country": "Gabon", "age": 88}, {"name": "Michael Hall", "country": "China", "age": 43}, {"name": "Sabrina Thompson", "country": "Chad", "age": 27}, {"name": "Jennifer Archer", "country": "Korea", "age": 45}, {"name": "Christina Simmons", "country": "Israel", "age": 80}, {"name": "Travis White", "country": "Central African Republic", "age": 31}, {"name": "Dennis Hernandez", "country": "Slovenia", "age": 66}, {"name": "Matthew Richards", "country": "Svalbard & Jan Mayen Islands", "age": 34}, {"name": "Stephen Curry", "country": "Finland", "age": 92}, {"name": "Margaret Williamson", "country": "Hong Kong", "age": 86}, {"name": "Mary Estes", "country": "Montenegro", "age": 19}, {"name": "Alex Scott", "country": "Christmas Island", "age": 67}, {"name": "John Andrews", "country": "Bahamas", "age": 68}, {"name": "Jonathan Willis", "country": "Saint Martin", "age": 23}, {"name": "Olivia Campos", "country": "Armenia", "age": 72}, {"name": "Diana Davis", "country": "Azerbaijan", "age": 54}, {"name": "Jack Cummings", "country": "Martinique", "age": 94}, {"name": "Kaitlyn Mcdonald", "country": "Austria", "age": 12}, {"name": "Maria Blake", "country": "Pitcairn Islands", "age": 91}, {"name": "Kelly Thomas", "country": "Ethiopia", "age": 74}, {"name": "John Terrell Jr.", "country": "India", "age": 50}, {"name": "Lindsay Wood", "country": "United Arab Emirates", "age": 72}, {"name": "Matthew Gilbert", "country": "Madagascar", "age": 86}, {"name": "Tanner Johnson", "country": "Congo", "age": 11}, {"name": "Michael Garcia", "country": "Liberia", "age": 45}, {"name": "Nicole Johnson", "country": "Barbados", "age": 54}, {"name": "William Lee", "country": "Lithuania", "age": 59}, {"name": "Jeffrey Coffey", "country": "Faroe Islands", "age": 88}, {"name": "Sandra Freeman", "country": "Philippines", "age": 35}, {"name": "Latoya Maxwell", "country": "Sweden", "age": 12}, {"name": "Darius Blevins", "country": "Thailand", "age": 29}, {"name": "Teresa Newman", "country": "Jersey", "age": 6}, {"name": "Larry Bray", "country": "Brunei Darussalam", "age": 21}, {"name": "Adam Roberson", "country": "Jordan", "age": 71}, {"name": "Michael Gomez", "country": "Tajikistan", "age": 37}, {"name": "Abigail Mccarthy", "country": "Kiribati", "age": 85}, {"name": "Tom Morris", "country": "Cayman Islands", "age": 27}, {"name": "Kevin Wagner", "country": "Suriname", "age": 55}, {"name": "Peggy Bryant", "country": "Korea", "age": 36}, {"name": "Erik Mclaughlin", "country": "Austria", "age": 24}]'
+# lst = json.loads(people)
+# lst = sorted(lst, key=lambda x: (x['age'], x['name']))
+# for i in lst:
+#     print(f'{i["name"]}, {i["country"]}, {i["age"]}')
+# -------------------
+# [print(f'{i["name"]}, {i["country"]}, {i["age"]}') for i in
+#  sorted(json.loads(people), key=lambda x: (x["age"], x["name"]))]
 
 
-
+# # 10.1 Выражения-генераторы
+#
+# Выражение генератор это почти тоже самое что и генератор списков, отличия лишь в скобках
+# Выражение генератор это – итератор обьекты которого можно обойти только один раз.
+# Итератор это обьект который поддерживает функцию next()
+# a = (i for i in range(1, 6))
+# print(next(a)) – выведется 1, данное выражение мы можем написать только 5 раз после чего уже не получится так как элементы генератора можно обойти только один раз.
+# Можно генератор обойти циклом for:
+# for i in a:
+#      print(i) -  Второй раз обойти уже не получится.
+# Можно найти сумму элементов генератора: print(sum(a))
+# Почему нельзя обойти второй раз? Потому-что элементы генератора попросту не хранятся в памяти.
+# Где это может пригодиться? Например если нужно сделать итератор с очень большой цифрой например 100000000
+# Такое большое число запихнуть в список через range() – не получится ибо выйдет ошибка memoryError
+# Но решение проблемы есть! – Генератор.
+# a = (i for i in range(1000000000)) – и никаких ошибок.
+# a = (i for i in range(10)) – его можно преобразовать в список:
+# print(list(a)) – [0,1,2,3,4,5,6,7,8,9] – список из генератора.
+# a = [(i for i in range(10))] – из этого список не выйдет.
