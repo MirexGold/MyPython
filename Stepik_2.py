bcab2ac5a57705a35edd673bb0454c585024c486
Polomoshnov <us_sr@mail.ru>
2022-07-25 23:54:09 +0700

    Stepic_2 ++

--- Stepik_2.py
+++ Stepik_2.py
@@ -1,12 +1,15 @@
-a = [3, 2, 1, 4, 6, 8]
+# list(map(int, input().split()))
+a = [1, 3, 5, 6, 10]
 b = []
 if len(a) == 1:
     print(*a)
 else:
     for i in a:
         if i == a[0]:
-            b.append(a[i+1]+a[-1])
+            b.append(a[1] + a[-1])
+        elif i == a[-1]:
+            b.append(a[-2] + a[0])
+        else:
+            b.append((a.index(i) - 1) + (a.index(i) + 1))
+
 print(b)
-
-
- # ([i + 1])+ ([-1])
