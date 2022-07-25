940f448f369e9ca0f9f9cdcc4a1caa18452a2d13
Polomoshnov <us_sr@mail.ru>
2022-07-25 22:10:53 +0700

    Stepic_2

--- main.py
+++ main.py
@@ -1,13 +1,1 @@
-a = [3, 2, 1, 4, 6, 8]
-b = []
-if len(a) == 1:
-    print(*a)
-else:
-    for i in a:
-        if i == a[0]:
-            b.append(a[i+1]+a[-1])
-print(b)
-
-
- # ([i + 1])+ ([-1])
 
