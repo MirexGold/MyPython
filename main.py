b7ee7c26aff1bb283983310d59134da669995c24
Polomoshnov <us_sr@mail.ru>
2022-07-25 22:08:19 +0700

    Жесть

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-import packege1
+a = [3, 2, 1, 4, 6, 8]
+b = []
+if len(a) == 1:
+    print(*a)
+else:
+    for i in a:
+        if i == a[0]:
+            b.append(a[i+1]+a[-1])
+print(b)
 
-print(packege1.)
+
+ # ([i + 1])+ ([-1])
+
