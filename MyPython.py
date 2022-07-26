3a767f5a88cbdb3d8de4be6a9706cecead85225f
Олег <us_sr@mail,ru>
2022-07-26 15:40:54 +0700

    Stepik 2.6.9

--- MyPython.py
+++ MyPython.py
@@ -7503,30 +7503,81 @@ from datetime import date  # -> импорт datetime
 
 # Игра Сапер
 
-n, m, k = (int(i) for i in input().split())  # чтение размеров поля и числа мин
-a = [[0 for j in range(m)] for i in range(n)]  # заполнение поля нулями
-for i in range(k):
-    row, col = (int(i) - 1 for i in input().split())
-    a[row][col] = -1  # расставляем мины
-for i in range(n):
-    for j in range(m):
-        if a[i][j] == 0:  # в этой клетке мины нет, поэтому считаем число мин вокруг
-            for di in range(-1, 2):
-                for dj in range(-1, 2):
-                    ai = i + di
-                    aj = j + dj
-                    # (ai, aj)
-                    if 0 <= ai < n and 0 <= aj < m and a[ai][aj] == -1:
-                        a[i][j] += 1
-# вывод результата
-for i in range(n):
-    for j in range(m):
-        if a[i][j] == -1:
-            print('*', end='')
-        elif a[i][j] == 0:
-            print('.', end='')
-        else:
-            print(a[i][j], end='')
-    print()
+# n, m, k = (int(i) for i in input().split())  # чтение размеров поля и числа мин
+# a = [[0 for j in range(m)] for i in range(n)]  # заполнение поля нулями
+# for i in range(k):
+#     row, col = (int(i) - 1 for i in input().split())
+#     a[row][col] = -1  # расставляем мины
+# for i in range(n):
+#     for j in range(m):
+#         if a[i][j] == 0:  # в этой клетке мины нет, поэтому считаем число мин вокруг
+#             for di in range(-1, 2):
+#                 for dj in range(-1, 2):
+#                     ai = i + di
+#                     aj = j + dj
+#                     # (ai, aj)
+#                     if 0 <= ai < n and 0 <= aj < m and a[ai][aj] == -1:
+#                         a[i][j] += 1
+# # вывод результата
+# for i in range(n):
+#     for j in range(m):
+#         if a[i][j] == -1:
+#             print('*', end='')
+#         elif a[i][j] == 0:
+#             print('.', end='')
+#         else:
+#             print(a[i][j], end='')
+#     print()
 
+# -----------------------------------
+
+# Программа, считывает числа (по одному в строке), пока сумма введённых чисел не будет равна 0
+# и сразу после этого выводит сумму квадратов всех считанных чисел
+
+# Заводим две переменные total - для подсчета += чисел
+# вторая с квадратами (+= x**2) Делаем  условие: while True: а дальше вводим число...
+# ищем total и одновременно считаем квадраты,
+# делаем условие, если total == 0: печатаем переменную,
+# считавшую квадраты и квадраты и break)))
+# a = int(input())
+# if a == 0:
+#     print(a)
+# else:
+#     count = a
+#     sq = a ** 2
+#     while True:
+#         a = int(input())
+#         sq += a ** 2
+#         count += a
+#         if count == 0:
+#             break
+#     sq = float(sq.real)
+#     print(int(sq))
+# # -------------------------------------
+# # -------------------------------------
+# # -------------------------------------
+# suma, sqd = 0, 0
+# while True:
+#     a = int(input())
+#     suma += a
+#     sqd += a**2
+#     if suma == 0:
+#         break
+# print(sqd)
+
+# --------------------------------
+# sp = [int(i) for i in input().split()]   #   2 3 5 9 8 2 ->  [2, 3, 5, 9, 8, 2]
+# sp = [i for i in range(int(input())+1)]    #   5   ->  [0, 1, 2, 3, 4, 5]
+
+# ------------------------------------------------------
+# Программа, выводит часть последовательности 1 2 2 3 3 3 4 4 4 4 5 5 5 5 5 ...
+# (число повторяется столько раз, чему равно)
+
+# sp = []
+# n = int(input())
+# for i in range(1, n+1):
+#     sp += [i] * i
+# print(*sp[0:n])
+
+# ----------------------------------------
 
