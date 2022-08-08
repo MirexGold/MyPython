c844a4b5cd1f2d0b1ca13c64f55c7212a6317e3b
Олег <us_sr@mail,ru>
2022-08-08 17:51:25 +0700

    11.2 Введение в списки

--- Generetion_Python.py
+++ Generetion_Python.py
@@ -1700,3 +1700,71 @@
 
 # 11.1 Введение в списки
 
+# print(list(range(1,int(input())+1)))
+
+# Список, состоящий из n букв английского алфавита ['a', 'b', 'c', ...]
+# print([chr(i) for i in range(97, 97+int(input()))])
+
+#           1  2  3  4  5   6   7   8   9   10  11  12  13  14  15  16  17  18  19  20
+# primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71]
+#           0  1  2  3  4   5   6   7   8   9   10  11  12  13  14  15  16  17  18  19
+# print(primes[16])
+
+
+# numbers = [12.5, 3.1415, 2.718, 9.8, 1.414, 1.1618, 1.324]
+# print(min(numbers)+max(numbers))
+
+
+# evens = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+# average = sum(evens)/len(evens)
+# print(average)
+
+
+# rainbow = ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Indigo', 'Violet']
+# rainbow[3] = 'Зеленый'
+# rainbow[-1] = 'Фиолетовый'
+#
+# print(rainbow)
+
+
+# numbers1 = [1, 2, 3]
+# numbers2 = [6]
+# numbers3 = [7, 8, 9, 10, 11, 12, 13]
+#
+# print(numbers1*2+numbers2*9+numbers3)
+
+# colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'brown', 'magenta']
+# del colors[2]
+# del colors[6]
+# print(colors)
+
+# numbers = [2, 6, 3, 14, 10, 4, 11, 16, 12, 5, 4, 16, 1, 0, 8, 16, 10, 10, 8, 5, 1, 11, 10, 10, 12, 0, 0, 6, 14, 8, 2,
+#            12, 14, 5, 6, 12, 1, 2, 10, 14, 9, 1, 15, 1, 2, 14, 16, 6, 7, 5]
+# print(len(numbers))  # Вывел длину списка;
+# print(numbers[-1])  # Вывел последний элемент списка;
+# print(numbers[::-1])  # Вывел список в обратном порядке (вспоминаем срезы);
+# print('YES' if ((5 or 17)) in numbers else 'NO')  # Вывел YES если список содержит числа 5 и 17, и NO в противном случае;
+# print(numbers[1:-1])            # Вывел список с удаленным первым и последним элементами.
+
+
+# Список строк
+# print([input() for i in range(int(input()))])
+
+# Алфавит
+
+# l = []
+# for i in range(ord('z') - ord('a') + 1):
+#     l.append(chr(ord('a') + i) * (i + 1))
+# print(l)
+#
+# print([chr(97 + i) * (i + 1) for i in range(26)])
+
+# Список кубов
+# ls = []
+# n = int(input())
+# for i in range(1,n+1):
+#     a = int(input())
+#     ls.append(a**3)
+# print(ls)
+
+# print([int(input())**3 for i in range(1,int(input())+1)])
