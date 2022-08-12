8a904088ebe1610e53ee328315080218b087699b
Олег <us_sr@mail,ru>
2022-08-12 17:54:05 +0700

    Generetion_Python_Next

--- Generetion_Python_Next.py
+++ Generetion_Python_Next.py
@@ -1,8 +1,50 @@
-# сумму чисел aa и bb;
-# разность чисел aa и bb;
-# произведение чисел aa и bb;
-# частное чисел aa и bb;
-# целую часть от деления числа aa на bb;
-# остаток от деления числа aa на bb;
-# корень квадратный из суммы их 10-х степеней: sqrt{a^{10} + b^{10}}
+# from math import sqrt
+# a, b = int(input()), int(input())
+# print(a + b)  # сумму чисел a и b;
+# print(a - b)  # разность чисел a и b;
+# print(a * b)  # произведение чисел a и b;
+# print(a / b)  # частное чисел a и b;
+# print(a // b)  # целую часть от деления числа a на b;
+# print(a % b)  # остаток от деления числа a на b;
+# print(sqrt(a**10 + b**10))  # корень квадратный из суммы их 10-х степеней: sqrt{a^{10} + b^{10}}
+
+# Индекс массы тела
+# def my_imt(m, r):
+#     imt = m / (r * r)
+#     if 18.5 <= imt <= 25:
+#         return "Оптимальная масса"
+#     elif imt < 18.5:
+#         return "Недостаточная масса"
+#     elif imt > 25:
+#         return "Избыточная масса"
+#
+#
+# m = float(input())
+# r = float(input())
+#
+# print(my_imt(m, r))
+
+
+# Стоимость строки
+# def coast(text):
+#     rub = len(text) * 0.6
+#     kop = len(text) * 60 % 100
+#     return f'{int(rub)} р. {kop} коп.'
+#
+# stroka = input()
+# print(coast(stroka))
+
+
+
+# string = input()
+# price = 60 * len(string)
+#
+# print(f'{price // 100} р. {price % 100} коп.')
+
+
+##Количество слов
+# print(len(list(input().split())))
+
+
+
 
