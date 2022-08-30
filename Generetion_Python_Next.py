827994b75994b903982005d42f2739a0fafba15c
Олег <us_sr@mail,ru>
2022-08-30 12:55:32 +0700

    13.3 Тип данных complex

--- Generetion_Python_Next.py
+++ Generetion_Python_Next.py
@@ -2994,5 +2994,177 @@
 # print(sorted_numbers)
 
 # 13.1 Модуль decimal
+# from decimal import *
+#
+# s = '9.73 8.84 8.92 9.60 9.32 8.97 8.53 1.26 6.62 9.85 1.85 1.80 0.83 6.75 9.74 9.11 9.14 5.03 5.03 1.34 3.52 8.09 7.89 8.24 8.23 5.22 0.30 2.59 1.25 6.24 2.14 7.54 5.72 2.75 2.32 2.69 9.32 8.11 4.53 0.80 0.08 9.36 5.22 4.08 3.86 5.56 1.43 8.36 6.29 5.13'
+# numbers = sorted([Decimal(i) for i in s.split()])
+# print(sum(numbers))
+# print(*numbers[-1:-6:-1])
+
+# from decimal import *
+# num = abs(Decimal(input()))
+# if num < 1:
+#     a = num.as_tuple().digits
+#     print(max(a))
+# else:
+#     print(max(num.as_tuple().digits) + min(num.as_tuple().digits))
+#
+#
+# from decimal import *
+# num = Decimal(input())
+# cyphers = num.as_tuple().digits
+# print(max(cyphers) + min(cyphers) * (abs(num) >= 1))
+
+# from decimal import *
+#
+# d = Decimal(input())
+# print(Decimal.sqrt(d) + Decimal.exp(d) + Decimal.ln(d) + Decimal.log10(d))
 
 
+# 13.2 Модуль fractions
+# from fractions import Fraction
+## num1 = Fraction(3, 4)     # 3 - числитель, 4 - знаменатель
+# num2 = Fraction('0.55')
+# num3 = Fraction('1/9')
+## print(num1, num2, num3, sep='\n')
+
+# from fractions import Fraction
+# num1 = Fraction(5, 1)        # 5/1 = 5
+# num2 = Fraction(23, 23)      # 23/23 = 1
+# print(num1, num2, sep='\n')
+
+
+# Cвойства numerator и denominator
+# Для получения числителя и знаменателя Fraction числа используются свойства numerator и denominator.
+# Приведенный ниже код:
+#
+# from fractions import Fraction
+# num = Fraction('5/16')
+# print('Числитель дроби равен:', num.numerator)
+# print('Знаменатель дроби равен:', num.denominator)
+# выводит:
+# Числитель дроби равен: 5
+# Знаменатель дроби равен: 16
+
+
+# В Python 3.8 появился метод as_integer_ratio(), который возвращает кортеж, состоящий из числителя
+# и знаменателя данного Fraction числа.
+# Приведенный ниже код:
+# from fractions import Fraction
+# num = Fraction('-5/16')
+# print(num.as_integer_ratio())
+# выводит:
+# (-5, 16)
+#
+# Метод limit_denominator()
+# Метод limit_denominator() возвращает самую близкую к данному числу рациональную дробь
+# чей знаменатель не превосходит переданного аргумента.
+# Приведенный ниже код:
+# from fractions import Fraction
+# import math
+# print('PI =', math.pi)
+# num = Fraction(str(math.pi))
+# print('No limit =', num)
+# for d in [1, 5,  50, 90, 100, 500, 1000000]:
+#     limited = num.limit_denominator(d)
+#     print(limited)
+# выводит:
+# PI = 3.141592653589793
+# No limit = 3141592653589793/1000000000000000
+# 3
+# 16/5
+# 22/7
+# 267/85
+# 311/99
+# 355/113
+# 3126535/995207
+# Метод limit_denominator() позволяет получить очень точные рациональные приближения иррациональных чисел,
+# что очень удобно во многих математических задачах.
+
+# Примечание 1. Для того, чтобы каждый раз не писать название типа, можно использовать следующий код:
+# from fractions import Fraction as F
+# num1 = F('1/5') + F('3/2')
+# num2 = F('1/4') * F('2/5')
+# print(num1)
+# print(num2)
+
+
+# from fractions import Fraction
+#
+# numbers = ['6.34', '4.08', '3.04', '7.49', '4.45', '5.39', '7.82', '2.76', '0.71', '1.97', '2.54', '3.67', '0.14',
+#            '4.29', '1.84', '4.07', '7.26', '9.37', '8.11', '4.30', '7.16', '2.46', '1.27', '0.29', '5.12', '4.02',
+#            '6.95', '1.62', '2.26', '0.45', '6.91', '7.39', '0.52', '1.88', '8.38', '0.75', '0.32', '4.81', '3.31',
+#            '4.63', '7.84', '2.25', '1.10', '3.35', '2.05', '7.87', '2.40', '1.20', '2.58', '2.46']
+#
+# # for i in numbers:
+# #     print(f'{i} = {Fraction(i)}')
+#
+# [print(f'{i} = {Fraction(i)}') for i in numbers]
+
+# вывел сумму наибольшего и наименьшего числа в виде обыкновенной дроби
+# from fractions import Fraction as F
+# from decimal import Decimal as D
+# s = '0.78 4.3 9.6 3.88 7.08 5.88 0.23 4.65 2.79 0.90 4.23 2.15 3.24 8.57 0.10 8.57 1.49 5.64 3.63 8.36 1.56 6.67 1.46 5.26 4.83 7.13 1.22 1.02 7.82 9.97 5.40 9.79 9.82 2.78 2.96 0.07 1.72 7.24 7.84 9.23 1.71 6.24 5.78 5.37 0.03 9.60 8.86 2.73 5.83 6.50 0.123 0.00021'
+# print(F(D(min(s.split())) + D(max(s.split()))))
+
+# # Сократите дробь
+# from fractions import Fraction as F
+# print(F(int(input()), int(input())))
+
+# Операции над дробями
+# from fractions import Fraction as F
+#
+# a, b = input(), input()
+# print(f'{a} + {b} = {F(a)+F(b)}')
+# print(f'{a} - {b} = {F(a)-F(b)}')
+# print(f'{a} * {b} = {F(a)*F(b)}')
+# print(f'{a} / {b} = {F(a)/F(b)}')
+
+
+# # Сумма дробей 1
+# from fractions import Fraction
+# n = int(input())
+# p = 0
+# for i in range(1, n + 1):
+#     a = Fraction(1, i) ** 2
+#     p += a
+# print(Fraction(p))
+
+# Сумма дробей 2
+# from fractions import Fraction as F
+#
+#
+# def fact(n):
+#     res = 1
+#     for i in range(1, n + 1): res *= i
+#     return res
+#
+#
+# n = int(input())
+# print(sum([F(1, fact(i)) for i in range(1, n + 1)]))
+
+# # Юный математик 🌶️
+# from fractions import Fraction
+# from math import gcd
+#
+# n = int(input())
+#
+# for i in range((n-1)//2, 0, -1):
+#     if gcd(i, n-i) == 1:
+#         print(Fraction(i, n-i))
+#         break
+
+# # Упорядоченные дроби
+# from math import gcd
+# from fractions import Fraction
+# n = int(input())
+# result = []
+# while n != 1:
+#     for i in range(1, n):
+#         if gcd(i, n) == 1:
+#             result.append(f'{i}/{n}')
+#     n -= 1
+# answer = sorted(map(Fraction, result))
+# print(*answer, sep='\n')
+
+# 13.3 Тип данных complex
